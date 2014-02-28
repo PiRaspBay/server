bd93d2154c989ebcb6def52b814a66eb1ad3c170
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-28 14:29:07 +0100

    request and relation mongodb colls and data

--- scripts/mongo-data.js
+++ scripts/mongo-data.js
@@ -1,6 +1,9 @@
 db.user.drop();
-
-db.user.ensureIndex({name:1},{unique:1});
+db.user.ensureIndex({
+  name: 1
+}, {
+  unique: 1
+});
 
 db.user.insert({
   name: 'val',
@@ -31,4 +34,58 @@ db.user.insert({
   key: '9Wjrb234h',
   raspIp: '62.129.6.2',
   lastSeen: NumberLong("1393592794000")
+});
+db.user.insert({
+  name: 'mamie',
+  key: '26fh62ra',
+  raspIp: '62.129.6.2',
+  lastSeen: NumberLong("1393592794000")
+});
+db.user.insert({
+  name: 'bob',
+  key: '99arfytuh3',
+  raspIp: '62.129.6.2',
+  lastSeen: NumberLong("1393592794000")
+});
+
+db.request.drop();
+db.request.ensureIndex({
+  from: 1,
+  to: 1
+}, {
+  unique: 1
+});
+
+[
+  ['tdu', 'val'],
+  ['jba', 'val'],
+  ['jba', 'tdu'],
+  ['the', 'val'],
+  ['the', 'tdu'],
+  ['tdu', 'the'],
+  ['mamie', 'tdu'],
+  ['bob', 'jba']
+].forEach(function(x) {
+  db.request.insert({from: x[0], to: x[1]});
+});
+
+db.relation.drop();
+db.relation.ensureIndex({
+  users: 1
+});
+
+[
+  ['tdu', 'renaud'],
+  ['jba', 'mamie'],
+  ['jba', 'renaud'],
+  ['mamie', 'renaud'],
+  ['mamie', 'val'],
+  ['mamie', 'the'],
+  ['mamie', 'bob'],
+  ['mamie', 'renaud'],
+  ['bob', 'tdu'],
+  ['bob', 'val'],
+  ['bob', 'the']
+].forEach(function(x) {
+  db.relation.insert({users: x});
 });
