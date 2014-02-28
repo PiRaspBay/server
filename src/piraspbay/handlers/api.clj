61f1a5fc2834674384d5cc8b9d7cbb2fb1ef3dbb
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-28 15:18:15 +0100

    public json user view

--- src/piraspbay/handlers/api.clj
+++ src/piraspbay/handlers/api.clj
@@ -7,38 +7,40 @@
 (defn online? [lastSeen]
   (time/before? (coerce/from-long lastSeen) (time/minus (time/now) (time/minutes 10))))
 
+(defn user-json [user] {:name (:name user)
+                        :online (online? (:lastSeen user))})
+
 (defn register [req]
   (let [key (-> req :params :key)
-       name (-> req :params :name)
-       address (-> req :remote-addr)]
-       (ping name key address (time/now))))
+        name (-> req :params :name)
+        address (-> req :remote-addr)]
+    (ping name key address (time/now))))
 
 (defn profile [req]
   (let [me (-> req :params :me)
-       name (-> req :params :user)
-       user (db/find-user name)]
-       {:name (:name user)
-        :online (online? (:lastSeen user))}))
+        name (-> req :params :user)
+        user (db/find-user name)]
+    (user-json user)))
 
 (defn friend [req]
   (let [me (-> req :params :me)]
-       (to-public (db/find-friends me))))
+    (map user-json (db/find-friends me))))
 
 (defn request [req]
   (let [me (-> req :params :me)]
-       [me]))
+    [me]))
 
 (defn accept [req]
   (let [me (-> req :params :me)
-       user (-> req :params :user)]
-       [me user]))
+        user (-> req :params :user)]
+    [me user]))
 
 (defn delete [req]
   (let [me (-> req :params :me)
-       user (-> req :params :user)]
-       [me user]))
+        user (-> req :params :user)]
+    [me user]))
 
 (defn new-request [req]
   (let [me (-> req :params :me)
-       user (-> req :params :user)]
-       [me user]))
+        user (-> req :params :user)]
+    [me user]))
